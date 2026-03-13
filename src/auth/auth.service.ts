20424aa118f2114ebf657dea6bd3f5636351dde6
saranrak <saranrak.d@ku.th>
2026-03-14 01:36:23 +0700

    local login api

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UserService } from '../user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
-
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  constructor(private userService: UserService) {}
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.userService.findByEmail(email);
+    console.log("user", user);
+    if (user && await bcrypt.compare(password, user.password)) {
+      const result = user.toObject();
+      return {
+        userId: result._id,
+        email: result.email,
+      };
+    }
+    return null;
   }
 }
